92c572dc926ae339ba3b00d2950ce5e3f9495231
liuli-neko <llhsdmd@gmail.com>
2023-06-11 14:01:17 +0800

    [testwindow][ui] add menu for items

--- src/tests/testwindow.cpp
+++ src/tests/testwindow.cpp
@@ -5,6 +5,7 @@
 #include <QDateTime>
 #include <QThreadPool>
 #include <QToolButton>
+#include <QMenu>
 
 #if defined(_WIN32)
 #include <Windows.h>
@@ -87,6 +88,7 @@ ZoodTestWindow::ZoodTestWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui
 	auto dui = static_cast<Ui::MainWindow*>(ui);
 
 	dui->setupUi(this);
+    setWindowFlag(Qt::Window);
 	QAction *clear = new QAction();
 	clear->setText("clear");
 
@@ -134,67 +136,96 @@ void ZoodTestWindow::showEvent(QShowEvent* event) {
 
 void ZoodTestWindow::RunAllTest() {
 	auto dui = static_cast<Ui::MainWindow*>(ui);
+
+    menu = new QMenu(dui->testList);
+    dui->testList->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(dui->testList, 
+            &QTreeWidget::customContextMenuRequested, 
+            this,[this](const QPoint &pos){
+                auto dui = static_cast<Ui::MainWindow*>(ui);
+                auto item = dui->testList->itemAt(pos);
+                if (item != nullptr) {
+                    ShowMenu(item, pos);
+                }
+            });
     
     // Register test
 	dui->testList->setHeaderLabels(QStringList{"测试列表", "完成进度"});
     QMap<QString, QTreeWidgetItem*> tree_items;
     dui->testList->setIndentation(10);
+    
     for (const auto &test_task : GetList()) {
         auto item_ = tree_items.find(test_task.module_name);
         if (item_ == tree_items.end()) {
             tree_items[test_task.module_name] = new QTreeWidgetItem(dui->testList, QStringList(test_task.module_name));
         }
+        
+        testModules[tree_items[test_task.module_name]].push_back(test_task.id);
+        testTasks[test_task.id] = test_task;
+
         auto item = new QTreeWidgetItem(tree_items[test_task.module_name], QStringList(test_task.test_name));
-        item->setText(1, "testing");
-        item->setForeground(1, QColor("#daa520"));
-		if (test_task.type == TestType::WIDGET) {
-            auto wi = static_cast<QWidget*>(test_task.task());
-            if (wi) {
-                auto widget = static_cast<QWidget*>(wi);
-                dui->testUiContainer->addWidget(widget);
-                items.insert(item, widget);
-            }
-            if (TestFlag(test_task.id) && wi) {
-                item->setText(1, "finished");
-                item->setForeground(1, QColor("#008000"));
-            } else {
-                item->setText(1, "failed");
-                item->setForeground(1, QColor("#b22222"));
-            }
-        } else if (test_task.type == TestType::WIDGET_W) {
-            auto wi = static_cast<QWidget*>(test_task.task());
-            if (wi) {
-                auto widget = static_cast<QWidget*>(wi);
-                widget->show();
-                widget->setAttribute(Qt::WA_DeleteOnClose);
-                connect(this, &QWidget::destroyed, widget, [widget](){
-                    widget->close();
-                });
-                connect(widget, &QWidget::destroyed, this, [item, test_task](){
-                    if (TestFlag(test_task.id)) {
-                        item->setText(1, "finished");
-                        item->setForeground(1, QColor("#008000"));
-                    } else {
-                        item->setText(1, "failed");
-                        item->setForeground(1, QColor("#b22222"));
-                    }
-                });
-            }
-        } else if (test_task.type == TestType::CMD) {
-            QMetaObject::invokeMethod(cmdHelperObject, [item, dui, test_task, this] {
-                test_task.task();
-                QMetaObject::invokeMethod(test_window, [=]() {
-                    if (TestFlag(test_task.id)) {
-                        item->setText(1, "finished");
-                        item->setForeground(1, QColor("#008000"));
-                    } else {
-                        item->setText(1, "falied");
-                        item->setForeground(1, QColor("#b22222"));
-                    }
-                });
-            });
-        }
+        items.insert(item, test_task.id);
+		RegisterTest(test_task, item);
 	}
+}
+
+void ZoodTestWindow::RegisterTest(const TestTask &task, QTreeWidgetItem* item) {
+    auto dui = static_cast<Ui::MainWindow*>(ui);
+
+    item->setText(1, "testing");
+    item->setForeground(1, QColor("#daa520"));
+
+    if (testWidgets.find(task.id) != testWidgets.end()) {
+        testWidgets[task.id]->deleteLater();
+        testWidgets[task.id] = nullptr;
+    }
+
+    if (task.type == TestType::WIDGET) {
+        auto wi = static_cast<QWidget*>(task.task());
+        if (wi) {
+            auto widget = static_cast<QWidget*>(wi);
+            dui->testUiContainer->addWidget(widget);
+            testWidgets[task.id] = widget;
+        }
+    } else if (task.type == TestType::WIDGET_W) {
+        auto wi = static_cast<QWidget*>(task.task());
+        if (wi) {
+            auto widget = static_cast<QWidget*>(wi);
+            widget->setParent(this);
+            widget->setWindowFlag(Qt::Window);
+            testWidgets[task.id] = widget;
+        }
+    } else if (task.type == TestType::CMD) {
+        QMetaObject::invokeMethod(cmdHelperObject, [item, dui, task, this] {
+            task.task();
+            QMetaObject::invokeMethod(test_window, [=]() {
+                if (TestFlag(task.id)) {
+                    item->setText(1, "finished");
+                    item->setForeground(1, QColor("#008000"));
+                } else {
+                    item->setText(1, "falied");
+                    item->setForeground(1, QColor("#b22222"));
+                }
+            });
+        });
+    }
+}
+
+void ZoodTestWindow::TestCompleter(QTreeWidgetItem* item) {
+    auto id = items[item];
+    auto task = testTasks[id];
+    if (TestFlag(task.id)) {
+        item->setText(1, "finished");
+        item->setForeground(1, QColor("#008000"));
+    } else {
+        item->setText(1, "falied");
+        item->setForeground(1, QColor("#b22222"));
+    }
+    auto obj = testWidgets[id];
+    if (obj != nullptr) {
+        obj->deleteLater();
+    }
+    testWidgets[id] = nullptr;
 }
 
 void ZoodTestWindow::ItemClicked(QTreeWidgetItem *item, int column) {
@@ -203,14 +234,126 @@ void ZoodTestWindow::ItemClicked(QTreeWidgetItem *item, int column) {
 	}
 	auto dui = static_cast<Ui::MainWindow*>(ui);
   
-	auto wi = items.find(item);
-	if (wi != items.end()) {
-        if ((*wi) && (*wi)->isWidgetType()) {
-    		dui->testUiContainer->setCurrentWidget(static_cast<QWidget*>(*wi));
-        }
+	auto id = items.find(item);
+	if (id != items.end()) {
+        ShowTestUi(*id);
 	}
 	currentItem = item;
 }
+
+void ZoodTestWindow::ShowTestUi(const uint64_t id) {
+	auto dui = static_cast<Ui::MainWindow*>(ui);
+
+    auto wi = testWidgets.find(id);
+    if (wi == testWidgets.end() || (*wi) == nullptr) {
+        return;
+    }
+    auto &task = testTasks[id];
+    if (task.type == TestType::WIDGET && (*wi)->isWidgetType()) {
+        dui->testUiContainer->setCurrentWidget(static_cast<QWidget*>(*wi));
+    } else if ((*wi)->isWidgetType()){
+        static_cast<QWidget*>(*wi)->show();
+    }
+}
+
+void ZoodTestWindow::RestartTest(QTreeWidgetItem* item) {
+    if (testModules.find(item) != testModules.end()) {
+        for (const auto id : testModules[item]) {
+            RegisterTest(testTasks[id], item);
+        }
+        return;
+    }
+    if (items.find(item) == items.end()) {
+        return;
+    }
+    auto id = items[item];
+    RegisterTest(testTasks[id], item);
+}
+
+void ZoodTestWindow::ShowTestUi(QTreeWidgetItem* item) {
+    auto dui = static_cast<Ui::MainWindow*>(ui);
+    dui->testList->setCurrentItem(item);
+
+    if (testModules.find(item) != testModules.end()) {
+        for (const auto id : testModules[item]) {
+            ShowTestUi(id);
+        }
+        return;
+    }
+    if (items.find(item) == items.end()) {
+        return;
+    }
+    auto id = items[item];
+    ShowTestUi(id);
+}
+
+void ZoodTestWindow::HideTestUi(QTreeWidgetItem* item) {
+    if (testModules.find(item) != testModules.end()) {
+        for (const auto id : testModules[item]) {
+            HideTestUi(id);
+        }
+        return;
+    }
+    if (items.find(item) == items.end()) {
+        return;
+    }
+    auto id = items[item];
+    HideTestUi(id);
+}
+
+void ZoodTestWindow::HideTestUi(const uint64_t id) {
+    auto dui = static_cast<Ui::MainWindow*>(ui);
+
+    auto wi = testWidgets.find(id);
+    if (wi == testWidgets.end() || nullptr == (*wi)) {
+        return;
+    }
+    auto &task = testTasks[id];
+    if (task.type == TestType::WIDGET_W && (*wi)->isWidgetType()){
+        static_cast<QWidget*>(*wi)->close();
+    }
+}
+
+void ZoodTestWindow::ShowMenu(QTreeWidgetItem* item, const QPoint& pos) {
+    auto dui = static_cast<Ui::MainWindow*>(ui);
+    QList<QAction*> actionList;
+    
+    if (testModules.find(item) != testModules.end()) {
+        actionList.push_back(menu->addAction("重新测试", [this, item, dui](){
+            RestartTest(item);
+            dui->testList->setCurrentItem(item);
+        }));
+    } else {
+        auto id = items.find(item);
+        if (id != items.end()) {
+            if (testTasks[*id].type == TestType::WIDGET_W && nullptr != testWidgets[*id]) {
+                actionList.push_back(menu->addAction("显示测试界面", [this, item](){
+                    ShowTestUi(item);
+                }));
+                actionList.push_back(menu->addAction("隐藏测试界面", [this, item](){
+                    HideTestUi(item);
+                }));
+                actionList.push_back(menu->addSeparator());
+            }
+            actionList.push_back(menu->addAction("重新测试", [this, item](){
+                    RestartTest(item);
+            }));
+            if (testTasks[*id].type != TestType::CMD && nullptr != testWidgets[*id]) {
+                actionList.push_back(menu->addAction("界面测试完成", [this, item](){
+                    TestCompleter(item);
+                }));
+            }
+        }
+    }
+
+    menu->exec(dui->testList->mapToGlobal(pos));
+
+    for (auto action : actionList) {
+        menu->removeAction(action);
+        action->deleteLater();
+    }
+}
+
 
 void ZoodTestWindow::keyPressEvent(QKeyEvent *event) {
 	if (event->key() == Qt::Key_F11) {
--- src/tests/testwindow.hpp
+++ src/tests/testwindow.hpp
@@ -8,6 +8,7 @@
 #include "../log.hpp"
 #include "testregister.hpp"
 
+struct TestTask;
 
 class ZoodTestWindow : public QMainWindow {
     Q_OBJECT
@@ -17,19 +18,33 @@ class ZoodTestWindow : public QMainWindow {
         ~ZoodTestWindow();
         QWidget* TerminatorParent();
         void RunAllTest();
+        void RegisterTest(const TestTask &task, QTreeWidgetItem* item);
 
     public:
         void showEvent(QShowEvent* event) override;
 
     public Q_SLOTS:
 		void ItemClicked(QTreeWidgetItem *item, int column);
+        void RestartTest(QTreeWidgetItem* item);
+        void ShowTestUi(QTreeWidgetItem* item);
+        void ShowTestUi(const uint64_t id);
+        void HideTestUi(QTreeWidgetItem* item);
+        void HideTestUi(const uint64_t id);
+        void TestCompleter(QTreeWidgetItem* item);
+        void ShowMenu(QTreeWidgetItem* item, const QPoint& pos);
+    
     protected:
         void keyPressEvent(QKeyEvent *) override;
+    
     private:
         void *ui = nullptr;
-		QMap<QTreeWidgetItem*, QObject*> items;
-		QTreeWidgetItem* currentItem = nullptr;
-        QThread        * cmdWorkerThread = nullptr;
-        QObject        * cmdHelperObject = nullptr;
+		QMap<QTreeWidgetItem*, uint64_t>        items;
+        QMap<uint64_t, QObject*>                testWidgets;
+        QMap<uint64_t, TestTask>                testTasks;
+        QMap<QTreeWidgetItem*, QList<uint64_t>> testModules;
+		QTreeWidgetItem*                        currentItem = nullptr;
+        QThread        *                        cmdWorkerThread = nullptr;
+        QObject        *                        cmdHelperObject = nullptr;
+        QMenu          *                        menu = nullptr;
     friend void ZoodLogString(const QString &text);
 };
